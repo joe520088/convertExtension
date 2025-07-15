3b0bf6e1c645279720bd776b1aca451e8cf1243a
Hao Ouyang <joe520088@gmail.com>
2025-07-15 12:41:17 -0500

    using the JSZip example by adding more thing to html and handling file not finish yet

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-DocumentTimeline.getElementById("fileInpiut").addEventListener('change',function(event)
+"use stric";
+var $result = $("#result");
+
+document.getElementById("file").addEventListener('change',function(event)
 {
-    const file = event.target.files[0];
-    if(file)
+    // getting the file from the drop box
+    const file = event.target.files;
+    // check for is there any file 
+    if(!file.length) return;
+
+    // making the part of html empty when the user input a new file
+    $result.html("");
+    // making sure the class is removed to completely wiped it off 
+    $("#result_block").removeClass("hidden").addClass("show");
+
+    // handle file and adding small header to show after the file is completed
+    function handeFile(f)
     {
-        console.log('File selected: ', file.name);
+        var $title = $("<h4>",
+            {
+                text : f.name
+            }
+        );
+        // after is finished is adding the file name to unorder list
+        var $fileContent = $("<ul>");
+        $result.append($title);
+        $result.append($fileContent);
+
     }
+
 });
